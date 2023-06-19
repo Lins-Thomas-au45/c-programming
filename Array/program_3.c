35ef6e304bf535bb68a8ab1382b710eb1093f121
Lins Thomas <raphealins@gmail.com>
2023-06-19 19:29:33 +0530

    sum problem

--- Array/program_3.c
+++ Array/program_3.c
@@ -14,7 +14,16 @@ Sum of all elements stored in the array is : 15
 
 int main(){
 
-    int arr[]
-
+    int arr[100],i,n,sum=0;
+    printf("Enter no of elements");
+    scanf("%d",&n);
+    printf("Enter the array elemenets");
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++){
+        sum+=arr[i];
+    }
+    printf("%d",sum);
     return 0;
 }
